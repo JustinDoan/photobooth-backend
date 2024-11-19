f52e419f7a55247e55f8fe5f69a9183327879867
Squeeshalami <squeeshalami@gmail.com>
2024-11-18 20:57:13 -0500

    added sliders for cfg & denoise

--- dto/image_dto.go
+++ dto/image_dto.go
@@ -3,9 +3,11 @@ package dto
 
 // ProcessImageRequest represents the request structure for processing an image
 type ProcessImageRequest struct {
-	Data   string `json:"data"` // Base64 encoded image data
-	Name   string `json:"name"` // Name of the image
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-	Prompt string `json:"prompt"`
+	Data              string  `json:"data"` // Base64 encoded image data
+	Name              string  `json:"name"` // Name of the image
+	Width             int     `json:"width"`
+	Height            int     `json:"height"`
+	Prompt            string  `json:"prompt"`
+	DenoisingStrength float64 `json:"denoisingStrength"`
+	CfgScale          float64 `json:"cfgScale"`
 }
