f77e87ea53305e1755c668b1d4209e2171d1b77b
Squeeshalami <squeeshalami@gmail.com>
2024-11-19 20:29:10 -0500

    Added loading bar for image progress

--- handlers/image_handler.go
+++ handlers/image_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/justindoan/photobooth-backend/controllers"
@@ -20,6 +21,7 @@ func NewImageHandler(imageController *controllers.ImageController) *ImageHandler
 	h.routes = []Route{
 		{http.MethodPost, "process", h.HandleProcessImage},
 		{http.MethodGet, "list", h.HandleListImages},
+		{http.MethodGet, "progress", h.HandleProgress},
 	}
 	return h
 }
@@ -78,4 +80,26 @@ func (h *ImageHandler) HandleListImages(w http.ResponseWriter, r *http.Request) 
 	// Send the response
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(images)
+}
+
+func (h *ImageHandler) HandleProgress(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	resp, err := http.Get("http://127.0.0.1:7860/sdapi/v1/progress")
+	if err != nil {
+		http.Error(w, "Failed to get progress", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(body); err != nil {
+		http.Error(w, "Failed to write response", http.StatusInternalServerError)
+		return
+	}
 }
